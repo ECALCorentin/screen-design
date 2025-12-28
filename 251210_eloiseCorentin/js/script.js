95028a750bce43e3baad5a1570575522c2850639
Corentin <corentin.maurer@ecal.ch>
2025-12-28 16:48:32 +0100

    project init

--- 251210_eloiseCorentin/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('ready');
-});
-
-// document.addEventListener('scroll', (event) => {
-// });
-
-// window.addEventListener('resize', (event) => {
-// });
